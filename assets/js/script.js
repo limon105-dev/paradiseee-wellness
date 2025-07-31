394a01e488e6baeaaabcffedc0ec5a6fd4718b69
md limon ali <limonali105@gmail.com>
2025-07-31 11:08:16 +0600

    project setup, navbar, hero, chat, icon-item, timeline, faq, product details, cta, footer section added

---
+++ assets/js/script.js
@@ -0,0 +1,61 @@
+$(document).ready(function () {
+	'use strict';
+	// navbar js start ---
+	$(window).on('load scroll', function () {
+		var scrolling = $(this).scrollTop();
+		if (scrolling > 10) {
+			$('.navbar').addClass('nav-fixed')
+		} else {
+			$('.navbar').removeClass('nav-fixed')
+		}
+	})
+
+	// $(".ham-menu").click(function (event) {
+	// 	event.stopPropagation();
+	// 	$(".nav-link-wrapper").toggleClass("active");
+	// 	$(".ham-menu").toggleClass("active");
+	// 	$(".nav-link-wrapper-inner").toggleClass("active");
+	// 	$("body").toggleClass("active");
+	// });
+
+	$(".mobile-menu-close-window-btn").click(function (event) {
+		event.stopPropagation();
+		$(".nav-link-wrapper").toggleClass("active");
+		$(".ham-menu").toggleClass("active");
+		$(".nav-link-wrapper-inner").toggleClass("active");
+		$("body").toggleClass("active");
+	});
+
+	// mobile-dropdown responsive accordion js start--
+	$(function () {
+		let isMobile = window.matchMedia("only screen and (max-width: 992px)").matches;
+
+		if (isMobile) {
+			$(".dropdown-sub-menu").slideUp();
+			// **..mobile-dropdown-accordion js start..**
+			$('.dropdown-btn1').on('click', function () {
+				$(this).next(".dropdown-sub-menu1").slideToggle();
+				$(this).toggleClass('active');
+			});
+			// **..mobile-dropdown-accordion js end..**
+
+			// **..mobile-dropdown-accordion js start..**
+			$('.dropdown-btn2').on('click', function () {
+				$(this).next(".dropdown-sub-menu2").slideToggle();
+				$(this).toggleClass('active');
+			});
+			// **..mobile-dropdown-accordion js end..**
+		}
+	});
+	// mobile-dropdown responsive accordion js end--
+
+	// navbar js end ---
+
+	// **..faq-accordion js start..**
+	$('.faq-accordion-toggle-btn').on('click', function () {
+		$(this).next().slideToggle();
+		$(this).parent().toggleClass('active');
+		$(this).parent().siblings().removeClass('active').find('.faq-accordion-item-content').slideUp();
+	});
+	// **..product-accordion js end..**
+})
